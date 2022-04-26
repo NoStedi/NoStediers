864ce0f1805489346ad50a0a519132f8730099c2
NoStedi <104290725+NoStedi@users.noreply.github.com>
2022-04-26 10:38:53 +0530

    Add files via upload

---
+++ d.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+curl -L "https://go.microsoft.com/fwlink/?LinkID=760868" > /tmp/vscode.deb
+sudo dpkg -i /tmp/vscode.deb && sudo apt-get install -f
+curl -o lab1.txt https://raw.githubusercontent.com/NoStedi/NoStediers/main/lab1.txt
+curl -o lab2.txt https://raw.githubusercontent.com/NoStedi/NoStediers/main/lab2.txt
+curl -o lab3.txt https://raw.githubusercontent.com/NoStedi/NoStediers/main/lab3.txt
+curl -o lab4.txt https://raw.githubusercontent.com/NoStedi/NoStediers/main/lab4.txt
+curl -o lab5.txt https://raw.githubusercontent.com/NoStedi/NoStediers/main/lab5.txt
+curl -o lab6.txt https://raw.githubusercontent.com/NoStedi/NoStediers/main/lab6.txt
+curl -o lab7.txt https://raw.githubusercontent.com/NoStedi/NoStediers/main/lab7.txt
+curl -o lab8.txt https://raw.githubusercontent.com/NoStedi/NoStediers/main/lab8.txt
+curl -o lab9.txt https://raw.githubusercontent.com/NoStedi/NoStediers/main/lab9.txt
+curl -o lab10.txt https://raw.githubusercontent.com/NoStedi/NoStediers/main/lab10.txt
+curl -o lab11.txt https://raw.githubusercontent.com/NoStedi/NoStediers/main/lab11.txt
